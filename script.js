bc60a710a7f10fae8e64af2838c41f0379722978
Nayanbarahate <nayanbarahate11@gmail.com>
2022-03-03 20:44:59 +0530

    adding two file change

---
+++ script.js
@@ -0,0 +1,111 @@
+//Decision Making 
+
+
+//if -else
+
+// let a=10;
+// if (a== 10){
+//     //success
+// }else{
+//     //failure
+// }
+
+
+//if - else ladder
+// let a=-10;
+// if (a<0){
+//     console.log("Negative");
+// }else if (a<0){
+//     console.log("zero");
+// }else{
+//     console.log("Postive");
+// }
+
+
+
+
+//switch-case statement
+
+// let gender = "m";
+// switch(gender){
+//     case "m":
+//         console.log("Male");
+//         break;
+//     case "f":
+//         console.log("Female");
+//         break;
+//     case "t":
+//         console.log("Transgender");
+//         break;    
+
+// }
+
+// var age=70;
+// switch(true){
+//     case age<18:
+//         console.log("child");
+//         break;
+//     case 18<age<40:
+//         console.log("Adult");    
+//         break;
+//     default:
+//         console.log("old age");
+//         break;   
+// }
+  
+
+//take var name day, its posible value are 0 1 2 3 4 5 .
+//0=monday 
+//5=Saturday
+
+
+// let Day = 5;
+
+
+// switch (Day) {
+//     case 0:
+//         console.log("Monday");
+//         break;
+//     case 1:
+//         console.log("Tuesday");
+//         break;
+//     case 2:
+//         console.log("Wednesday");
+//         break;
+//     case 3:
+//         console.log("Thursday");
+//         break;
+//     case 4:
+//         console.log("Friday");
+//         break;
+//      case 5:
+//         console.log("Saturday");
+//         break;
+//     default:
+//         console.log("Sunday");
+//         break;
+// }
+
+
+
+////loops ;;;;;
+
+//loops- for , while , do while ,
+
+// let i;
+// for(i=1; i<=10; i++)
+// {
+//     console.log(i);
+// }
+
+//var a;
+//declaring a variable
+//var a=12;
+//we are intializing the variable with the value of 12;
+
+
+// for (let i=1; i<11; i++){
+//     console.log("19*" + i+"=" + 19*i);
+// }
+
+
